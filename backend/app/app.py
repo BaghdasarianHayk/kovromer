73c73701e862caca3240083e8cb9221152271d95
Hayk Baghdasarian <baghdasarianhayk@gmail.com>
2024-11-29 00:22:20 +0400

    add cors and nginx websocket

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.workers.router import router as worker_router
 from app.cameras.router import router as camera_router
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(worker_router)
 app.include_router(camera_router)
