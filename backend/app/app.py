f0deaaa1daf25bfd46c7179754eb8ba3b3cfc99b
Hayk Baghdasarian <baghdasarianhayk@gmail.com>
2024-11-28 13:16:59 +0400

    add camera router

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.workers.router import router as worker_router
-
+from app.cameras.router import router as camera_router
 app = FastAPI()
 
 app.include_router(worker_router)
+app.include_router(camera_router)
