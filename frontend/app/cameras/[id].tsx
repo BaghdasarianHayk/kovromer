847724939dcf9b6c80c4fabdbd24a6642c0173a7
Hayk Baghdasarian <baghdasarianhayk@gmail.com>
2024-11-29 04:11:46 +0400

    fix camera in frontend

--- frontend/app/cameras/[id].tsx
+++ frontend/app/cameras/[id].tsx
@@ -1,69 +1,102 @@
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState, useRef } from 'react';
+import { Button, Text, View, SafeAreaView } from 'react-native';
+import MainButton from '../../components/MainButton';
+import { useAuth } from '../../context/AuthContext';
+import { router, useLocalSearchParams } from 'expo-router';
+import ProtectedRoute from '../../components/ProtectedRoute';
 
-export default function App() {
+export default function Cameras() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [isStreaming, setIsStreaming] = useState(false);
+  const cameraRef = useRef<CameraView | null>(null);
+  const ws = useRef<WebSocket | null>(null);
+  const frameInterval = useRef<NodeJS.Timeout | null>(null);
+  const { currentToken } = useAuth();
+  const { id } = useLocalSearchParams()
+
+  const startStreaming = async () => {
+    if (!ws.current || ws.current.readyState !== WebSocket.OPEN) {
+
+      ws.current = new WebSocket(`wss://ковромер.рф/api/cameras/${id}/ws?token=${encodeURIComponent(currentToken)}`);
+
+      ws.current.onopen = () => {
+        console.log('WebSocket connected');
+        setIsStreaming(true);
+        captureAndSendFrames();
+      };
+
+      ws.current.onclose = () => {
+        console.log('WebSocket closed');
+        setIsStreaming(false);
+      };
+
+      ws.current.onerror = (error) => {
+        console.error('WebSocket error:', error);
+      };
+    }
+  };
+
+  const stopStreaming = () => {
+    if (ws.current) {
+      ws.current.close();
+    }
+    setIsStreaming(false);
+
+    if (frameInterval.current) {
+      clearInterval(frameInterval.current); // Clear the interval
+      frameInterval.current = null; // Reset the interval reference
+    }
+    router.replace('/cameras')
+  };
+
+  const captureAndSendFrames = async () => {
+    if (cameraRef.current) {
+      frameInterval.current = setInterval(async () => {
+        const photo = await cameraRef.current.takePictureAsync({
+          base64: true,
+          quality: 0.4,
+          exif: false,
+          fastMode: true,
+          scale: 0.4
+        });
+
+        if (ws.current && ws.current.readyState === WebSocket.OPEN) {
+          ws.current.send(photo.base64);
+        }
+      }, 100);
+    }
+  };
 
   if (!permission) {
-    // Camera permissions are still loading.
     return <View />;
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
     return (
-      <View style={styles.container}>
-        <Text style={styles.message}>We need your permission to show the camera</Text>
+      <View>
+        <Text>We need your permission to show the camera</Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
   }
 
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
-  }
-
   return (
-    <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </CameraView>
-    </View>
+    <ProtectedRoute>
+      <View style={{flex: 1, backgroundColor: 'white', width: '100%', height: '100%'}}>
+        <SafeAreaView style={{flex: 1, width: '100%', height: '100%'}}>
+            <CameraView style={{flex: 1}} facing={facing}>
+              <View style={{position: 'absolute', bottom: 20, alignItems: 'center', width: '100%'}}>
+                {isStreaming ? (
+                  <MainButton defaultWidth={320} type="danger" text="Прекратить трансляцию" onPress={stopStreaming} />
+                ) : (
+                  <MainButton defaultWidth={320} type="standard" text="Начать трансляцию" onPress={startStreaming} />
+                )}
+              </View>
+            </CameraView>
+        </SafeAreaView>
+      </View>
+    </ProtectedRoute>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  message: {
-    textAlign: 'center',
-    paddingBottom: 10,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
